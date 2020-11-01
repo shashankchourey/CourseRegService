d3695f3183d3dfcc288e0891d9df6d168072ffcf
Shashank Chourey <chourey.shashank21@gmail.com>
2020-11-01 11:40:05 -0800

    20201101_2: Added Comment

--- src/main/java/com/shashank/springboot/application/hello/BusinessHelloController.java
+++ src/main/java/com/shashank/springboot/application/hello/BusinessHelloController.java
@@ -3,6 +3,7 @@ package com.shashank.springboot.application.hello;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// This is a Rest Controller for the Business Hello Class with request mappings 
 @RestController
 public class BusinessHelloController {
 
