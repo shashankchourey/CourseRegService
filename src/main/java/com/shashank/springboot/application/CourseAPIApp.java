f96b42dbda6f91a2188a4589e258efad3a79581d
Shashank Chourey <chourey.shashank21@gmail.com>
2020-11-01 11:53:07 -0800

    20200111_3: Edit Comment

--- src/main/java/com/shashank/springboot/application/CourseAPIApp.java
+++ src/main/java/com/shashank/springboot/application/CourseAPIApp.java
@@ -3,14 +3,12 @@ package com.shashank.springboot.application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//This is the main class to trigger the application,
+//Once it starts, Spring class path scan starts looking for classes
+
 @SpringBootApplication
 public class CourseAPIApp {
 	public static void main(String args[]) {
 		SpringApplication.run(CourseAPIApp.class, args);
 	}
 }
-
-// This is the main class to trigger the application,
-// Once it starts, Spring class path scan starts looking for classes
-
-
