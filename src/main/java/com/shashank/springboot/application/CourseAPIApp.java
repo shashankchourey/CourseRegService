04cc23be8ee46ad6f40d592564d2b9a703a252e5
Shashank Chourey <chourey.shashank21@gmail.com>
2022-11-12 16:54:56 -0800

    Add test comment

--- src/main/java/com/shashank/springboot/application/CourseAPIApp.java
+++ src/main/java/com/shashank/springboot/application/CourseAPIApp.java
@@ -5,7 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 //This is the main class to trigger the application,
 //Once it starts, Spring class path scan starts looking for classes
-
+//test comment 1
+// 
 @SpringBootApplication
 public class CourseAPIApp {
 	public static void main(String args[]) {
